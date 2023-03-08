917944b44db7ec4201582f51f29f93cb7a4263df
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-03-08 22:09:22 +0600

    assignment done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var arrayPlayer = [];
 var count = 0;
 
-function buyNowbtn(element)
+function buyNowbtn(element,buttonId)
 {
    
 
@@ -28,13 +28,74 @@ function buyNowbtn(element)
     `
     playerBody.appendChild(tr);
 
+
+
+    document.getElementById(buttonId).disabled = true;
+
     
 
 }
 
-function disableButton(buttonId) {
-    document.getElementById(buttonId).disabled = true;
-  }
+// function disableButton(buttonId) {
+
+    
+//   }
+
+  // Get all the buttons on the page
+
+//   const buttons = document.querySelectorAll('button');
+
+//   // Loop through each button and add an event listener to disable it when clicked
+//   buttons.forEach(button => {
+//     button.addEventListener('click', function() {
+//       button.disabled = true;
+//     });
+//   });
+
+
+
+document.getElementById('calculator_btn').addEventListener('click',function()
+
+{
+    const perPlayerString = document.getElementById('per_player_input').value;
+    const perPlayer = parseFloat(perPlayerString);
+    
+
+    const playerExpenses = document.getElementById('player_expen');
+    playerExpenses.innerText = perPlayer;
+   
+
+    
+
+
+})
+
+document.getElementById('total_cal_btn').addEventListener('click',function()
+{
+
+    const playerExpenses = document.getElementById('player_expen');
+    const expensesString = playerExpenses.innerText;
+    const expenses = parseFloat(expensesString);
+
+
+    const managerInputFiled = document.getElementById('manager_input').value;
+    const mangerInput = parseFloat(managerInputFiled);
+
+    const coachInputFiled = document.getElementById('coach_input').value;
+    const coachinput = parseFloat(coachInputFiled);
+
+    const totalAmount =  mangerInput + coachinput +expenses;
+
+    const total = document.getElementById('total_display');
+    total.innerText = totalAmount;
+})
+ 
+  
+  
+  
+  
+  
+  
  
   
   
